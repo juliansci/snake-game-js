9a6eae4aecd1719a663cb95d05ada90f7957e2e7
Julián Scialabba <julian.scialabba@gmail.com>
2020-08-16 02:52:35 -0300

    Move some methods to Snake

--- js/Game.js
+++ js/Game.js
@@ -3,125 +3,28 @@ import Board from './Board.js';
 import Snake, { SNAKE_DIRECTIONS } from './Snake.js';
 import Food from './Food.js';
 
-
-
-
 function Game(blocksHorizontal, blocksVertical) {
   this.board = new Board(blocksHorizontal, blocksVertical);
   this.drawer = new Drawer(this.board);
   this.snake = new Snake(this.board.getRandomPositionAvailable());
   this.food = new Food(this.board.getRandomPositionAvailable(this.snake));
 
-  const snakeMove = () => {
-    const { body: snakeBody } = this.snake
-    for (let i = snakeBody.length - 1; i >= 0; i--) {
-      const snakeBlock = snakeBody[i];
-      if (i === 0) {
-        snakeMoveHead(snakeBlock);
-      } else {
-        const snakeNextBlock = snakeBody[i - 1];
-        snakeMoveBody(snakeBlock, snakeNextBlock);
-      }
-    }
-  }
-
-  const snakeMoveHead = (snakeHead) => {
-    const { blocksHorizontal, blocksVertical } = this.board;
-    const { direction: snakeDirection } = this.snake;
-    switch (snakeDirection) {
-      case SNAKE_DIRECTIONS.UP:
-        if (snakeHead[1] > 0) {
-          snakeHead[1] -= 1;
-        } else {
-          snakeHead[1] = blocksVertical - 1;
-        }
-        break;
-      case SNAKE_DIRECTIONS.DOWN:
-        if (snakeHead[1] < blocksVertical - 1) {
-          snakeHead[1] += 1;
-        } else {
-          snakeHead[1] = 0;
-        }
-        break;
-      case SNAKE_DIRECTIONS.LEFT:
-        if (snakeHead[0] > 0) {
-          snakeHead[0] -= 1;
-        } else {
-          snakeHead[0] = blocksHorizontal - 1;
-        }
-        break;
-      case SNAKE_DIRECTIONS.RIGHT:
-        if (snakeHead[0] < blocksHorizontal - 1) {
-          snakeHead[0] += 1;
-        } else {
-          snakeHead[0] = 0;
-        }
-        break;
-    }
-  }
-
-  const snakeMoveBody = (snakeBlock, snakeNextBlock) => {
-    snakeBlock[0] = snakeNextBlock[0];
-    snakeBlock[1] = snakeNextBlock[1];
-  }
-
-  const growSnake = () => {
-    const { body, direction } = this.snake;
-    const snakeTail = body[body.length - 1];
-    switch (direction) {
-      case SNAKE_DIRECTIONS.UP:
-        body.push([snakeTail[0], snakeTail[1] + 1]);
-        break;
-      case SNAKE_DIRECTIONS.DOWN:
-        body.push([snakeTail[0], snakeTail[1] - 1]);
-        break;
-      case SNAKE_DIRECTIONS.LEFT:
-        body.push([snakeTail[0] + 1, snakeTail[1]]);
-        break;
-      case SNAKE_DIRECTIONS.RIGHT:
-        body.push([snakeTail[0] - 1, snakeTail[1]]);
-        break;
-    }
-  }
-
-  const checkSnakeEat = () => {
-    const { body: snakeBody } = this.snake;
-    const { position: foodPosition } = this.food;
-
-    const snakeHead = snakeBody[0];
-    if (snakeHead[0] === foodPosition[0] && snakeHead[1] === foodPosition[1]) {
-      growSnake();
-      this.food.position = this.board.getRandomPositionAvailable(this.snake, this.food);
-    }
-  }
-  const checkSnakeColission = () => {
-    const { body } = this.snake;
-    if (body.length <= 1) return false;
-    const snakeHead = body[0];
-    for (let i = 1; i < body.length; i++) {
-      const snakeBlock = body[i];
-      if (snakeHead[0] === snakeBlock[0] && snakeHead[1] === snakeBlock[1]) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-
   const animate = () => {
-    snakeMove();
-    const snakeColission = checkSnakeColission();
-    if (snakeColission) {
+    this.snake.move(this.board);
+    const snakeInColission = this.snake.checkCollision();
+    if (snakeInColission) {
       clearInterval(this.intervalAnimation);
       this.snake.isDead = true;
       this.drawer.drawGame(this.snake, this.food);
-      console.log('Perdiste!');
     }
-    checkSnakeEat();
+    const snakeEatFoot = this.snake.checkEatFood(this.food);
+    if (snakeEatFoot) {
+      this.food.position = this.board.getRandomPositionAvailable(this.snake, this.food);
+    }
     this.drawer.drawGame(this.snake, this.food);
   }
 
-  const handleKeyEvent = () => {
+  const handleKeyEvents = () => {
     const onKeyDown = function (e) {
       e = e || window.event;
       const { direction: snakeDirection } = this.snake;
@@ -171,17 +74,10 @@ function Game(blocksHorizontal, blocksVertical) {
     document.onkeydown = onKeyDown.bind(this);
   }
 
-  const createBoard = (blocksHorizontal, blocksVertical) => {
-    return {
-      blocksHorizontal,
-      blocksVertical
-    }
-  };
-
   const start = () => {
     this.drawer.drawGame(this.snake, this.food);
     this.intervalAnimation = setInterval(animate, 130);
-    handleKeyEvent();
+    handleKeyEvents();
   }
 
   return {
--- js/Snake.js
+++ js/Snake.js
@@ -9,6 +9,102 @@ function Snake(position) {
   this.body = [position];
   this.direction = SNAKE_DIRECTIONS.RIGHT;
   this.isDead = false;
+
+
+
+  this.move = board => {
+    const snakeBody = this.body;
+    for (let i = this.body.length - 1; i >= 0; i--) {
+      const snakeBlock = snakeBody[i];
+      if (i === 0) {
+        _moveHead(snakeBlock, board);
+      } else {
+        const snakeNextBlock = snakeBody[i - 1];
+        _moveBody(snakeBlock, snakeNextBlock);
+      }
+    }
+  }
+
+  this.checkEatFood = (food) => {
+    const { position: foodPosition } = food;
+    const snakeHead = this.body[0];
+    if (snakeHead[0] === foodPosition[0] && snakeHead[1] === foodPosition[1]) {
+      grow();
+      return true;
+    }
+    return false;
+  }
+
+  this.checkCollision = () => {
+    const { body } = this;
+    if (body.length <= 1) return false;
+    const snakeHead = body[0];
+    for (let i = 1; i < body.length; i++) {
+      const snakeBlock = body[i];
+      if (snakeHead[0] === snakeBlock[0] && snakeHead[1] === snakeBlock[1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const grow = () => {
+    const { body, direction } = this;
+    const snakeTail = body[body.length - 1];
+    switch (direction) {
+      case SNAKE_DIRECTIONS.UP:
+        body.push([snakeTail[0], snakeTail[1] + 1]);
+        break;
+      case SNAKE_DIRECTIONS.DOWN:
+        body.push([snakeTail[0], snakeTail[1] - 1]);
+        break;
+      case SNAKE_DIRECTIONS.LEFT:
+        body.push([snakeTail[0] + 1, snakeTail[1]]);
+        break;
+      case SNAKE_DIRECTIONS.RIGHT:
+        body.push([snakeTail[0] - 1, snakeTail[1]]);
+        break;
+    }
+  }
+
+  const _moveHead = (snakeHead, board) => {
+    const { blocksHorizontal, blocksVertical } = board;
+    switch (this.direction) {
+      case SNAKE_DIRECTIONS.UP:
+        if (snakeHead[1] > 0) {
+          snakeHead[1] -= 1;
+        } else {
+          snakeHead[1] = blocksVertical - 1;
+        }
+        break;
+      case SNAKE_DIRECTIONS.DOWN:
+        if (snakeHead[1] < blocksVertical - 1) {
+          snakeHead[1] += 1;
+        } else {
+          snakeHead[1] = 0;
+        }
+        break;
+      case SNAKE_DIRECTIONS.LEFT:
+        if (snakeHead[0] > 0) {
+          snakeHead[0] -= 1;
+        } else {
+          snakeHead[0] = blocksHorizontal - 1;
+        }
+        break;
+      case SNAKE_DIRECTIONS.RIGHT:
+        if (snakeHead[0] < blocksHorizontal - 1) {
+          snakeHead[0] += 1;
+        } else {
+          snakeHead[0] = 0;
+        }
+        break;
+    }
+  }
+
+  const _moveBody = (snakeBlock, snakeNextBlock) => {
+    snakeBlock[0] = snakeNextBlock[0];
+    snakeBlock[1] = snakeNextBlock[1];
+  }
 }
 
 export default Snake;
